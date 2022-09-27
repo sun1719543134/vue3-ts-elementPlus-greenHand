b31e9f59457f4b94a3091198b9916d5433bfa12d
sun1719543134 <sun171954134@163.com>
2022-09-27 17:36:49 +0800

    使用axios时全局加载

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,45 +1,18 @@
+import requestCurrency from '@/utils/request-currency'//统一处理错误信息的接口,不需要写 catch
 import request from '@/utils/request'
-import { ElMessage } from 'element-plus'
-interface infoOne extends infoTow {
-    url: string;
-    method?: string;
-}
+//调取接口可能传的值
 interface infoTow {
     params?: object;
 }
-
-function getTest3(row1: infoOne) {
-    return new Promise((resolve, reject) => {
-        request({
-            url: row1.url,//此处为自己请求地址
-            method: row1.method,
-            params: row1.params
-        }).then(res1 => {
-            console.log('res1', res1)
-            resolve(res1)
-        }).catch(error => {
-            if (error.status) {
-                switch (error.status) {
-                    default:
-                        ElMessage.error('status:' + error.status)
-                }
-            }
-            console.log('error', error)
-            resolve(error)
-        })
-    })
-}
+//get获取淘宝时间
 export function getTest1(row1: infoTow) {
-    return getTest3({
+    return requestCurrency({
         url: '/rest/api3.do',//此处为自己请求地址
         method: 'get',
         params: row1.params
     })
-    // getTest3({
-    //     url: '/rest/api3.do?api=mtop.common.getTimestamp',//此处为自己请求地址
-    //     method: 'get',
-    // })
 }
+//get获取淘宝时间
 export function getTest2(params: object) {
     return request({
         url: '/rest/api3.do',//此处为自己请求地址
---
+++ src/utils/request-currency.ts
@@ -0,0 +1,34 @@
+import request from '@/utils/request'
+import { ElMessage, ElLoading } from 'element-plus'//全局加载 提示框
+//调取接口需要的数据继承vue传过来的值
+interface infoOne {
+    url: string;
+    method?: string;
+    params?: object;
+}
+//调取axios接口
+function requestCurrency(row1: infoOne) {
+    return new Promise((resolve, reject) => {
+        ElLoading.service({ fullscreen: true })
+        request({
+            url: row1.url,//此处为自己请求地址
+            method: row1.method,
+            params: row1.params
+        }).then(res1 => {
+            ElLoading.service().close()
+            console.log('res1', res1)
+            resolve(res1)
+        }).catch(error => {
+            ElLoading.service().close()
+            if (error.status) {
+                switch (error.status) {
+                    default:
+                        ElMessage.error('status:' + error.status)
+                }
+            }
+            console.log('error', error)
+            resolve(error)
+        })
+    })
+}
+export default requestCurrency
