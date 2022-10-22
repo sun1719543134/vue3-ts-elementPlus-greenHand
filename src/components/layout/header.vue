e3726dd35fd47390295de8afa92ba58d1bfe4066
sun1719543134 <sun171954134@163.com>
2022-10-22 13:39:24 +0800

    优化

--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -1,24 +1,27 @@
 <template>
-    <div style="display: flex;height: 100%;">
-        <div style="padding: 7px;">
+    <div class="outermost-layer">
+        <div class="sidebar-toggle">
             <el-icon size="26px" @click="isCollapseTF()">
                 <i-ep-Expand v-if="layoutStore.isCollapse"></i-ep-Expand>
                 <i-ep-Fold v-else></i-ep-Fold>
             </el-icon>
         </div>
-        <div style="flex: 1;"></div>
-        <div style="padding: 10px;">
+        <div class="fill-flex"></div>
+        <div class="diablo-toggle">
             <el-icon size="20px" @click="toggleDark()">
                 <i-ep-Moon v-if="isDark"></i-ep-Moon>
                 <i-ep-Sunny v-else></i-ep-Sunny>
             </el-icon>
         </div>
-        <div style="padding: 10px;">
-            <el-icon size="20px" @click="setUseLanguage()">
+        <div class="language-switching">
+            <div @click="setUseLanguage()">
+                {{locale=='en'?'中':'En'}}
+            </div>
+            <!-- <el-icon size="20px" @click="setUseLanguage()">
                 <i-ep-HelpFilled></i-ep-HelpFilled>
-            </el-icon>
+            </el-icon> -->
         </div>
-        <div @click="exitLogin()" style="line-height: 40px;">
+        <div @click="exitLogin()" class="log-out">
             <!-- <el-avatar :size="24" :src="''">
                 <img src="@/assets/anime-avatar-1.png" alt="">
             </el-avatar> -->
@@ -77,6 +80,42 @@ const exitLogin = () => {
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+/* 最外层 */
+.outermost-layer {
+    display: flex;
+    height: 100%;
 
+    //填充flex
+    .fill-flex {
+        flex: 1;
+    }
+
+    // 侧边栏切换
+    .sidebar-toggle {
+        padding: 7px;
+    }
+
+    // 暗黑模式切换
+    .diablo-toggle {
+        padding: 10px;
+    }
+
+    // 语言切换
+    .language-switching {
+        padding: 10px;
+        font-weight: 700;
+
+        div {
+            width: 25px;
+            text-align: center;
+        }
+    }
+
+    // 退出登录
+    .log-out {
+        padding-left: 10px;
+        line-height: 40px;
+    }
+}
 </style>
