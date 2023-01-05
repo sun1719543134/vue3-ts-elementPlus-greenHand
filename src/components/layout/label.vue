e6d5148ef9d592cf67508b0f9098befd0cf75d6a
虚拟度过一生一世 <sun1719543134@163.com>
2023-01-05 17:56:50 +0800

    1

--- src/components/layout/label.vue
+++ src/components/layout/label.vue
@@ -1,25 +1,5 @@
 <template>
   <div style="height: 41px;user-select:none;">
-    <!-- <div style="position: relative;width: 100%;height: 100%;">
-      <el-icon style="position: absolute;height: 100%;width: 30px;border-right: 1px solid var(--el-border-color);">
-        <i-ep-ArrowLeftBold></i-ep-ArrowLeftBold>
-      </el-icon>
-      <el-icon
-        style="position: absolute;height: 100%;width: 30px;right: 0;border-left: 1px solid var(--el-border-color);">
-        <i-ep-ArrowRightBold></i-ep-ArrowRightBold>
-      </el-icon>
-      <div style="position: absolute;left: 35px;right: 35px;top: 3px;bottom: 0;">
-        <el-scrollbar ref="scrollbarRef">
-          <div style="display: flex;">
-            <el-tag v-for="tag, index1 of layoutStore.labelCache" :key="index1" :closable="tag.name != 'home'"
-              size="large" :type="tag.name == routerName ? '' : 'info'" style="margin-right:5px;"
-              @close="closeLabel(tag, index1)" @click="clickTheTab(tag)">
-              {{ tag.name }}
-            </el-tag>
-          </div>
-        </el-scrollbar>
-      </div>
-    </div> -->
     <el-tabs v-model="routerName" type="card" class="demo-tabs" :before-leave="clickTheTab" @edit="closeLabel">
       <el-tab-pane v-for="tag, index1 of layoutStore.labelCache" :label="tag.meta.title" :name="tag.name" :key="index1"
         :closable="tag.name != 'home'"></el-tab-pane>
